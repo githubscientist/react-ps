91b5aecb8f4334b47faa798a5da532029cf91442
githubscientist <sathish.datascientist@gmail.com>
2024-10-09 18:11:45 +0530

    state handling in class components

--- src/App.jsx
+++ src/App.jsx
@@ -12,11 +12,19 @@ class App extends React.Component {
 
   // method override to render the component
   render() {
+
+    // method to increase the count
+    const increase = () => {
+      this.setState({
+        count: this.state.count + 1,
+      });
+    }
+
     return (
       <div>
         <h1>Counter: {this.state.count}</h1>
         <button
-          onClick={() => this.setState({ count: this.state.count + 1 })}
+          onClick={increase}
         >Increase</button>
       </div>
     )
