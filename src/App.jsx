e9917d67909222165a238fa723df9f0764eabf98
githubscientist <sathish.datascientist@gmail.com>
2024-10-09 17:43:15 +0530

    props drilling

--- src/App.jsx
+++ src/App.jsx
@@ -1,6 +1,36 @@
-const App = () => {
+// props drilling: passing data from parent component to child component through props (properties) in multiple levels
+// child component of A: B
+const B = ({ name }) => {
   return (
-    <div>App</div>
+    <div>
+      <h1>Hello, {name}!</h1>
+    </div>
+  )
+}
+
+// child component of App: A
+const A = ({ name }) => {
+  return (
+    <div>
+      <B
+        name={name}
+      />
+    </div>
+  )
+}
+
+// parent component: App
+// data is in the parent component
+// data: name
+const App = () => {
+  const name = 'React';
+
+  return (
+    <div>
+      <A
+        name={name}
+      />
+    </div>
   )
 }
 
