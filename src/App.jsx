0a8ff38bcd725004c8b37d66627bf2ccf0fbf572
githubscientist <sathish.datascientist@gmail.com>
2024-10-09 17:56:48 +0530

    class component simple example

--- src/App.jsx
+++ src/App.jsx
@@ -1,37 +1,10 @@
-// props drilling: passing data from parent component to child component through props (properties) in multiple levels
-// child component of A: B
-const B = ({ name }) => {
-  return (
-    <div>
-      <h1>Hello, {name}!</h1>
-    </div>
-  )
-}
+import React from 'react';
 
-// child component of App: A
-const A = ({ name }) => {
-  return (
-    <div>
-      <B
-        name={name}
-      />
-    </div>
-  )
-}
-
-// parent component: App
-// data is in the parent component
-// data: name
-const App = () => {
-  const name = 'React';
-
-  return (
-    <div>
-      <A
-        name={name}
-      />
-    </div>
-  )
+class App extends React.Component {
+  // method override to render the component
+  render() {
+    return <h1>Hello, world!</h1>;
+  }
 }
 
 export default App;
